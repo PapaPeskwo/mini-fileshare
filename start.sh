44227540a9aa8e919b872962648d609e02463892
PapaPeskwo <tomi.orlovac@gmail.com>
2024-09-09 16:59:45 +0200

    feat: :sparkles: refactor start.sh script for improved readability and efficiency

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 python3 -m http.server 23645 --directory ./shared &
-ngrok authtoken $NGROK_AUTH_TOKEN
-ngrok http 23645 &
+lt --port 23645 > tunnel_info.txt &
 sleep 5
-NGROK_URL=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
-echo "Your public URL is: $NGROK_URL"
+PUBLIC_URL=$(grep -o 'https://.*' tunnel_info.txt | head -n 1)
+echo "Your public URL is: $PUBLIC_URL"
 tail -f /dev/null
