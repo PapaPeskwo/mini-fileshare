98ed4e91486fc0295fa217925e0d825ffe962d16
PapaPeskwo <tomi.orlovac@gmail.com>
2024-09-13 10:16:42 +0200

    feat: :sparkles: delete start.sh script for local server setup

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m http.server 23645 --directory ./shared &
-lt --port 23645 > tunnel_info.txt &
-sleep 5
-PUBLIC_URL=$(grep -o 'https://.*' tunnel_info.txt | head -n 1)
-echo "Your public URL is: $PUBLIC_URL"
-tail -f /dev/null
